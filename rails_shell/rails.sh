8307028077138f95b1b489c439cdab0a30800d73
Nathan Broadbent <git@ndbroadbent.com>
2018-04-18 15:25:57 +0700

    Run rails with bundle exec

--- rails_shell/rails.sh
+++ rails_shell/rails.sh
@@ -1,11 +1,11 @@
 # Run Rails commands on any version
 rails_cmd(){
   # Rails 3
-  if [ -e ./script/rails ]; then bundle_install_wrapper rails3_with_editor "$@"
+  if [ -e ./script/rails ]; then bundle_install_wrapper bundle exec rails3_with_editor "$@"
   # Rails <= 2
   elif [ -e ./script/$1 ]; then bundle_install_wrapper "./script/$@"
   # Rails 4
-  elif [ -e ./config.ru ] && grep -q Rails ./config.ru; then bundle_install_wrapper rails "$@"
+  elif [ -e ./config.ru ] && grep -q Rails ./config.ru; then bundle_install_wrapper bundle exec rails "$@"
   else echo "== I don't think this is a Rails application!"
   fi
 }
