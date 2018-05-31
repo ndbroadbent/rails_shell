aaad8d6dcc8d09c602bfc512876a320cf74763df
Nathan Broadbent <git@ndbroadbent.com>
2018-05-31 17:42:42 +0700

    Moved to dotfiles

--- rails_shell/rubygems.sh
+++
@@ -1,4 +0,0 @@
-alias gmi="gem install"
-alias gml="gem list"
-alias gmb="gem build"
-alias gmd="cd $GEM_HOME/gems"
--- rails_shell/vagrant.sh
+++
@@ -1,4 +0,0 @@
-alias vu="vagrant up"
-alias vs="vagrant ssh"
-alias vr="vagrant reload"
-alias vh="vagrant halt"
